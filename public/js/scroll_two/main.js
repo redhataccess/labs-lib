983e55dde28718c976445ca1b497141239d44654
Xiumei Meng <xmeng@redhat.com>
2015-01-28 15:30:05 +0800

    Scroll_two module added.

---
+++ public/js/scroll_two/main.js
@@ -0,0 +1,17 @@
+/*global labs_define, window*/
+labs_define('scroll_two', ['jquery'], function (jq) {
+    "use strict";
+
+    var priv = {},
+        pub = {};
+
+    pub.to_id = function (id) {
+        setTimeout(function () {
+            jq('html,body').animate({
+                scrollTop: jq(id).offset().top
+            }, 'fast');
+        }, 100);
+    };
+
+    return pub;
+});
